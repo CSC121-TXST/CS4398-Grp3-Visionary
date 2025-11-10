24c1ff4addd96a052f6b9b58751698fa72562d3f
Chris Clark <csc121@txstate.edu>
2025-11-09 18:06:48 -0600

    Detection/Tracking Repo

--- src/vision/tracking.py
+++ src/vision/tracking.py
@@ -4,4 +4,6 @@ Object Tracking Module
 This module handles tracking of detected objects across frames
 to enable smooth camera movement and object following.
 
+Pulling Code from the following GITHUB Repo: https://github.com/MuhammadMoinFaisal/YOLOv8-DeepSORT-Object-Tracking/blob/main/README.md
+
 """
